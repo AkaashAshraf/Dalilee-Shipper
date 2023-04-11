51e18b8df893f52d06258f604a9be37f823c1e67
AkaashAshraf <akash@thiqatech.com>
2023-04-11 14:44:41 +0400

    ....

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 // const primaryColor = Colors.green;
-bool isTesEnvironment = false;
+bool isTesEnvironment = true;
 double omrToAedRate = 9.47;
 const int iosVersionLocal = 6; //10/april/2023 //otp for enq added
 const int androidVersionLocal = 6; //10/april/2023 //otp for enq added
 
-Color primaryColor = Color(isTesEnvironment ? 0xFF76529c : 0xFF00A1E1);
+Color primaryColor = Color(isTesEnvironment ? 0xFF0D7297 : 0xFF00A1E1);
 
 const whiteColor = Color(0xFFFFFFFF);
 const bgColor = Color(0xFFF8F8F8);
 const text1Color = Color(0xFF61656A);
-Color textBlueColor = Color(isTesEnvironment ? 0xFF76529c : 0xFF00A1E1);
+Color textBlueColor = Color(isTesEnvironment ? 0xFF0D7297 : 0xFF00A1E1);
 const textRedColor = Color(0xFFD32F2F);
 const cardColor = Color(0xFFF6F6F6);
 
@@ -20,8 +20,9 @@ const cardColor = Color(0xFFF6F6F6);
 
 // String domain = "https://shaheen-oman.dalilee.om"; // live
 
-String domain =
-    isTesEnvironment ? "http://test.meshwarr.com" : "https://shaheenom.com"; //
+String domain = isTesEnvironment
+    ? "https://staging.shaheenom.com"
+    : "https://shaheenom.com"; //
 
 String base_url = "$domain/api"; // live
 String terms_url = "$domain/terms"; // live
