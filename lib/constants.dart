deaab17367858571ac6a5f96c01898faa38864d5
AkaashAshraf <akash131060@gmail.com>
2023-09-26 14:03:59 +0400

    v16

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,9 +3,8 @@ import 'package:flutter/material.dart';
 // const primaryColor = Colors.green;
 bool isTesEnvironment = false;
 double omrToAedRate = 9.47;
-const int iosVersionLocal = 11; //14/june/2023   download ref orders
-const int androidVersionLocal = 11; //14/june/2023   download ref orders
-
+const int iosVersionLocal = 16; //26/Sep/2023  force update because of shipper app version
+const int androidVersionLocal =  16; //26/Sep/2023  force update because of shipper app version
 Color primaryColor = Color(isTesEnvironment ? 0xFF76529c : 0xFF00A1E1);
 
 const whiteColor = Color(0xFFFFFFFF);
